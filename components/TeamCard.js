12bfcaefb4cbb244ac636e302345db62999177ae
skulldanger5533 <158171243+BhaveshYadavIITBHU@users.noreply.github.com>
2024-12-28 13:30:15 +0530

    checked, props.member in vertical names needs corr

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -5,12 +5,13 @@ export default function TeamCard(props) {
     return (
         <div>
             <h2 className='text-[#189BA5] text-xl text-center'>{props.vertical}  Team</h2>
-            <div className='text-center p-2'>
+            <div className='text-center p-2' >
                 <p className='p-1 text-lg text-center'>{props.member1}</p>
                 <p className='p-1 text-lg text-center'>{props.member2}</p>
                 <p className='p-1 text-lg text-center'>{props.member3}</p>
                 <p className='p-1 text-lg text-center'>{props.member4}</p>
                 <p className='p-1 text-lg text-center'>{props.member5}</p>
+             
             </div>
         </div>
     )
