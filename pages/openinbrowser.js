c0311ca048b038074fe40ac4d6001cc84b8c70ea
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2025-01-11 00:49:05 +0530

    Update openinbrowser.js

--- pages/openinbrowser.js
+++ pages/openinbrowser.js
@@ -2,7 +2,7 @@ export default function OpenInBrowser(){
 
     return (
         <div className="mt-12 block mx-auto">
-            <a className="block mx-auto" target="_blank" href="https://iitbhumun.com">Open In External Browser</a>
+            <a className="block mx-auto" rel="noreferrer" target="_blank" href="https://iitbhumun.com">Open In External Browser</a>
         </div>
     )
 }
