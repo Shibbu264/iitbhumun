ade08d266f640cebace2639ba55286667aa3a352
rishabhr4 <118053976+rishabhr4@users.noreply.github.com>
2025-01-11 00:32:49 +0530

    created openinbrowser

---
+++ pages/openinbrowser.js
@@ -0,0 +1,8 @@
+export default function OpenInBrowser(){
+
+    return (
+        <div className="mt-12 block mx-auto">
+            <a className="block mx-auto" target="_blank" href="https://iitbhumun.com">Open In External Browser</a>
+        </div>
+    )
+}
